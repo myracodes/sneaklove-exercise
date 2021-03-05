2b49d2eda428f1691cbfd08e90e56160a3c79346
Juan Falconi <juanfalconi86@gmail.com>
2021-03-05 12:12:09 +0100

    addeed tags models and sneakers model and seed files. Added some Tags into Mongo DB

---
+++ models/Sneaker.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const sneakerSchema = new Schema({
+  name: String,
+  ref: String,
+  size: Number,
+  description: String,
+  price: Number,
+  category: {
+    type: String,
+    enum: ["men", "women", "kids"],
+  },
+  id_tags: [{ type: Schema.Types.ObjectId, ref: "tags" }],
+});
+
+const SneakerModel = mongoose.model("sneakers", sneakerSchema);
+
+module.exports = SneakerModel;
