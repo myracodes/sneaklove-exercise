4d4e7f8e747b8375769eff860edd19744d14ffb1
fmmyriam <benhmyriam@gmail.com>
2021-03-05 17:27:51 +0100

    dashboard and create method and routes

--- routes/dashboard_sneaker.js
+++ routes/dashboard_sneaker.js
@@ -1,4 +1,44 @@
 const express = require("express"); // import express in this module
-const router = new express.Router(); // create an app sub-module (router)
+const router = express.Router(); // create an app sub-module (router)
+
+
+
+// create dashboard sneakers
+router.get('/prod-add', (req, res, next) => {
+    res.render("products_add.hbs");
+  });
+  
+  router.post('/prod-add', async (req, res, next) => {
+    console.log(req.body);
+    const {
+      name,
+      ref,
+      size,
+      description,
+      price,
+      category
+    } = req.body;
+    console.log(name, ref, size,
+        description,
+        price,
+        category);
+    try {
+         await SneakerModel.create({
+        name,
+        ref,
+        size,
+        description,
+        price,
+        category,
+      });
+      console.log("shoe successfully created :D");
+      res.redirect("/sneakers/collection");
+    } catch (err) {
+      next(err);
+      res.render("error.hbs");
+    }
+  });
+
+
 
 module.exports = router;
